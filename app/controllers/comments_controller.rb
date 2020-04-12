a8810905e9791d5adc2c72b18529ee7754b068dc
Matthew Jones <matts3371@gmail.com>
2020-04-12 17:19:21 -0500

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,9 @@
 class CommentsController < ApplicationController
 
   def create
+    if params[:comment][:user_id].blank?
+      params[:comment].delete(:user_id)
+    end
     comment = Comment.create(comment_params)
     redirect_to comment.post
   end
@@ -10,4 +13,5 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
   end
+
 end
